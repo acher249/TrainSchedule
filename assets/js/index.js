2294eae6909f2b0fb61eafac2fe99ab29d27a23a
acher249 <32597382+acher249@users.noreply.github.com>
2018-07-25 18:50:41 -0400

    Moment Update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,96 +1,104 @@
-var employeeName = $("#employeeName").val();
-var role = $("#role").val();
-var startDate = $("#startDate").val();
-var monthlyRate = $("#monthlyRate").val();
+$(document).ready(function() {
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDgKmB8p6TjDPQ7v89KSPDcIw0Rk9eXHyw",
-    authDomain: "employeedatamgmt-9168b.firebaseapp.com",
-    databaseURL: "https://employeedatamgmt-9168b.firebaseio.com",
-    projectId: "employeedatamgmt-9168b",
-    storageBucket: "",
-    messagingSenderId: "27645932124"
-};
-
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-database.ref().on("child_added", function(snapshot) {
-
-    console.log("This DB event function is firing");
-
-    //dynamically create the HTML
-    var tableBody = $("#tableBody")
-    var newTable = $("<tr>");
-    var tdEmployeeName = $("<td>");
-    var tdRole = $("<td>");
-    var tdStartDate = $("<td>");
-    var tdMonthsWorked = $("<td>");
-    var tdMonthlyRate = $("<td>");
-    var tdTotalBilled = $("<td>");
-
-    //Append all of the newly created divs to the right place
-    tableBody.append(newTable);
-    newTable.append(tdEmployeeName);
-    newTable.append(tdRole);
-    newTable.append(tdStartDate);
-    newTable.append(tdMonthsWorked);
-    newTable.append(tdMonthlyRate);
-    newTable.append(tdTotalBilled);
-
-    //Store what you get back from the DB
-    employeeNameDB = snapshot.val().employeeName;
-    roleDB = snapshot.val().role;
-    startDateDB = snapshot.val().startDate;
-    monthlyRateDB = snapshot.val().monthlyRate;
-
-    //calculate months worked
-    var momentFormat = "MM/DD/YYYY";
-    var convertedDate = moment(startDateDB, momentFormat);
-    var monthsWorked = moment(convertedDate).diff(moment(), "months");
-    var monthsWorked = -monthsWorked;
-
-    //calculate total $ billed
-    var totalBilled = monthsWorked * monthlyRateDB;
+    var employeeName = $("#employeeName").val();
+    var role = $("#role").val();
+    var startDate = $("#startDate").val();
+    var monthlyRate = $("#monthlyRate").val();
     
-    //Add the DB data to the HTML
-    tdEmployeeName.text(employeeNameDB);
-    tdRole.text(roleDB);
-    tdStartDate.text(startDateDB);
-    tdMonthlyRate.text(monthlyRateDB);
-    tdMonthsWorked.text(monthsWorked);
-    tdTotalBilled.text(totalBilled);
-
-}, function(errorObject) {
-console.log("The read failed: " + errorObject.code);
-});
-
-$("#submitButton").on("click", function(event) {
-
-    employeeName = $("#employeeName").val();
-    role = $("#role").val();
-    startDate = $("#startDate").val();
-    monthlyRate = $("#monthlyRate").val();
-
-    console.log(employeeName);
-    console.log(role);
-    console.log(startDate);
-    console.log(monthlyRate);
-
-    database.ref().push({
-        employeeName: employeeName,
-        role: role,
-        startDate: startDate,
-        monthlyRate: monthlyRate,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCjtvd1OZm6BCpaJcFznRSvyIs1n9rS788",
+        authDomain: "trainschedule-b0085.firebaseapp.com",
+        databaseURL: "https://trainschedule-b0085.firebaseio.com",
+        projectId: "trainschedule-b0085",
+        storageBucket: "trainschedule-b0085.appspot.com",
+        messagingSenderId: "566567800628"
+      };
+    
+      firebase.initializeApp(config);
+    
+    var database = firebase.database();
+    
+    database.ref().on("child_added", function(snapshot) {
+    
+        console.log("This DB event function is firing");
+    
+        //dynamically create the HTML
+        var tableBody = $("#tableBody")
+        var newTable = $("<tr>");
+        var tdEmployeeName = $("<td>");
+        var tdRole = $("<td>");
+        var tdStartDate = $("<td>");
+        var tdMonthsWorked = $("<td>");
+        var tdMonthlyRate = $("<td>");
+        var tdTotalBilled = $("<td>");
+    
+        //Append all of the newly created divs to the right place
+        tableBody.append(newTable);
+        newTable.append(tdEmployeeName);
+        newTable.append(tdRole);
+        newTable.append(tdStartDate);
+        newTable.append(tdMonthsWorked);
+        newTable.append(tdMonthlyRate);
+        newTable.append(tdTotalBilled);
+    
+        //Store what you get back from the DB
+        employeeNameDB = snapshot.val().employeeName;
+        roleDB = snapshot.val().role;
+        startDateDB = snapshot.val().startDate;
+        monthlyRateDB = snapshot.val().monthlyRate;
+    
+        //calculate months worked
+        var momentFormat = "MM/DD/YYYY";
+        var convertedDate = moment(startDateDB, momentFormat);
+        var monthsWorked = moment(convertedDate).diff(moment(), "months");
+        var monthsWorked = -monthsWorked;
+        console.log(monthsWorked);
+    
+        //calculate total $ billed
+        var totalBilled = monthsWorked * monthlyRateDB;
+        console.log(totalBilled);
+        
+        //Add the DB data to the HTML
+        tdEmployeeName.text(employeeNameDB);
+        tdRole.text(roleDB);
+        tdStartDate.text(startDateDB);
+        tdMonthlyRate.text(monthlyRateDB);
+        tdMonthsWorked.text(monthsWorked);
+        tdTotalBilled.text(totalBilled);
+    
+    }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
     });
+    
+    $("#submitButton").on("click", function(event) {
+    
+        employeeName = $("#employeeName").val();
+        role = $("#role").val();
+        startDate = $("#startDate").val();
+        monthlyRate = $("#monthlyRate").val();
+    
+        console.log(employeeName);
+        console.log(role);
+        console.log(startDate);
+        console.log(monthlyRate);
+    
+        database.ref().push({
+            employeeName: employeeName,
+            role: role,
+            startDate: startDate,
+            monthlyRate: monthlyRate,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+    });
+    
+    $("#clearButton").on("click", function(event) {
+        employeeName = $("#employeeName").val("");
+        role = $("#role").val("");
+        startDate = $("#startDate").val("");
+        monthlyRate = $("#monthlyRate").val("");
+    });
+
 });
 
-$("#clearButton").on("click", function(event) {
-    employeeName = $("#employeeName").val("");
-    role = $("#role").val("");
-    startDate = $("#startDate").val("");
-    monthlyRate = $("#monthlyRate").val("");
-});
+
