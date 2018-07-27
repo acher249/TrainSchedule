4db78572dbfa82817f6532b3269ad02a618d26b1
Adam <jchernick2010@gmail.com>
2018-07-26 20:58:49 -0400

    clean up
    clean up

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
     var frequency = $("#frequency").val();
 
     var currentTime = moment();
+    debug.log("CURRENT TIME: " + currentTime);
+
 
     document.getElementById("trainName").focus();
     
@@ -22,12 +24,15 @@ $(document).ready(function() {
       firebase.initializeApp(config);
     
     var database = firebase.database();
+
+    var childKey = "";
     
     //PULL EVERYTHING BACK FROM DB
     //Child added function also gets run on load
     database.ref().on("child_added", function(snapshot) {
     
         // console.log("child_added DB event function is firing");
+        childKey = snapshot.key;
     
         //dynamically create the HTML
         var tableBody = $("#tableBody")
@@ -47,6 +52,7 @@ $(document).ready(function() {
         //delete button
         var tdDeleteButton = $("<td>");
         var deleteButton = $("<button>")
+        deleteButton.attr("id", "deleteButton");
         var deleteImg = $("<img>")
         var deleteImgUrl = "https://png.icons8.com/metro/1600/delete.png"
         deleteImg.attr("src", deleteImgUrl);
@@ -110,6 +116,13 @@ $(document).ready(function() {
     console.log("The read failed: " + errorObject.code);
     });
 
+    $(document).on('click', '#deleteButton', function(){
+        console.log("delete button clicked");
+        //Two steps: delete out of DB then delete out of html
+        // var rowToDelete= document.getElementById(childKey);
+        // rowToDelete.parentNode.removeChild(rowToDelete);
+    });
+
     //This is listening when an item get removerd from DB, update the HTML
     //Need to figure out how to remove from DB from the site
     database.ref().on("child_removed", function(snapshot) {
@@ -117,9 +130,7 @@ $(document).ready(function() {
         const changedItems = document.getElementById(snapshot.key)
         changedItems.remove();
         console.log("Deleted Item from DB");
-    
-    }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
+
     });
     
     // PUSH EVERYTHING TO DB
@@ -163,103 +174,103 @@ $(document).ready(function() {
 //# Smooth Doodle
 //Demonstrates the use of `toxi.geom.Spline2D`
 
-window.onload = init;
-var gui = new dat.GUI();
-var canvas, ctx, spline, points;
+// window.onload = init;
+// var gui = new dat.GUI();
+// var canvas, ctx, spline, points;
 
-var params = {
-    distance: 60,
-    tightness: 0.25
-};
+// var params = {
+//     distance: 60,
+//     tightness: 0.25
+// };
 
-function init(){
-    canvas = document.getElementById('example');
-    // canvas.style.backgroundColor = e9ecef;
-    ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - 100;
-    spline = new toxi.geom.Spline2D();
-    points = [];
-    //controls
-    gui.add( params, 'distance', 5, 200 );
-    gui.add( params, 'tightness', 0.001, 0.5).step(0.025).onChange(function(){
-        spline.setTightness( params.tightness );
-    });
-    gui.add({
-        clear: function(){ ctx.clearRect(0,0,canvas.width,canvas.height); }
-    }, 'clear');
+// function init(){
+//     canvas = document.getElementById('example');
+//     // canvas.style.backgroundColor = e9ecef;
+//     ctx = canvas.getContext('2d');
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight - 100;
+//     spline = new toxi.geom.Spline2D();
+//     points = [];
+//     //controls
+//     gui.add( params, 'distance', 5, 200 );
+//     gui.add( params, 'tightness', 0.001, 0.5).step(0.025).onChange(function(){
+//         spline.setTightness( params.tightness );
+//     });
+//     gui.add({
+//         clear: function(){ ctx.clearRect(0,0,canvas.width,canvas.height); }
+//     }, 'clear');
 
-    window.onmousemove = function(e){
-        update(e.pageX,e.pageY);
-    };
-}
+//     window.onmousemove = function(e){
+//         update(e.pageX,e.pageY);
+//     };
+// }
 
 
-function update(mouseX,mouseY){
-    var numP=points.length;
-    var currP=new toxi.geom.Vec2D(mouseX,mouseY);
-    if (numP>0) {
-        var prevP=points[numP-1];
-        if (currP.distanceTo(prevP)> params.distance) {
-            points.push(currP);
-            ctx.strokeStyle = "rgba(0,0,0,.5)";
-            ctx.beginPath();
-            //this creates circle
-            ctx.arc(currP.x,currP.y,10,0,Math.PI*2);
-            ctx.stroke();
-            ctx.closePath();
-            spline.add(currP);
-            if (numP > 1) {
-                var lastP = points[numP-2];
-                var p = points[numP-1];
-                line(lastP.x,lastP.y,p.x,p.y);
-                ctx.strokeStyle = "rgba(0,0,0,0.5)";
-                ctx.arc(p.x,p.y,7,0,Math.PI*2);
-            }
-            // need at least 4 vertices for a spline
-            if (numP>3) {
-                ctx.strokeStyle = "rgba(0,0,0,.125)";
-                // sample the curve at a higher resolution
-                // so that we get extra 8 points between each original pair of points
-                var vertices=spline.computeVertices(8);
-                // draw the smoothened curve
-                ctx.beginPath();
-                var numRecent = Math.max(vertices.length-64,0);
-                for(var i=numRecent;i<vertices.length;i++) {
-                    var v = vertices[i];
-                    if(i == numRecent){
-                        ctx.moveTo(v.x,v.y);
-                    }
-                    else {
-                        ctx.lineTo(v.x,v.y);
-                    }
-                }
-                ctx.stroke();
-                ctx.closePath();
-            }
-        }
-    }
-    else {
-        points.push(currP);
-    }
-}
+// function update(mouseX,mouseY){
+//     var numP=points.length;
+//     var currP=new toxi.geom.Vec2D(mouseX,mouseY);
+//     if (numP>0) {
+//         var prevP=points[numP-1];
+//         if (currP.distanceTo(prevP)> params.distance) {
+//             points.push(currP);
+//             ctx.strokeStyle = "rgba(0,0,0,.5)";
+//             ctx.beginPath();
+//             //this creates circle
+//             ctx.arc(currP.x,currP.y,10,0,Math.PI*2);
+//             ctx.stroke();
+//             ctx.closePath();
+//             spline.add(currP);
+//             if (numP > 1) {
+//                 var lastP = points[numP-2];
+//                 var p = points[numP-1];
+//                 line(lastP.x,lastP.y,p.x,p.y);
+//                 ctx.strokeStyle = "rgba(0,0,0,0.5)";
+//                 ctx.arc(p.x,p.y,7,0,Math.PI*2);
+//             }
+//             // need at least 4 vertices for a spline
+//             if (numP>3) {
+//                 ctx.strokeStyle = "rgba(0,0,0,.125)";
+//                 // sample the curve at a higher resolution
+//                 // so that we get extra 8 points between each original pair of points
+//                 var vertices=spline.computeVertices(8);
+//                 // draw the smoothened curve
+//                 ctx.beginPath();
+//                 var numRecent = Math.max(vertices.length-64,0);
+//                 for(var i=numRecent;i<vertices.length;i++) {
+//                     var v = vertices[i];
+//                     if(i == numRecent){
+//                         ctx.moveTo(v.x,v.y);
+//                     }
+//                     else {
+//                         ctx.lineTo(v.x,v.y);
+//                     }
+//                 }
+//                 ctx.stroke();
+//                 ctx.closePath();
+//             }
+//         }
+//     }
+//     else {
+//         points.push(currP);
+//     }
+// }
 
-function line(x1,y1,x2,y2){
-    ctx.beginPath();
-    ctx.strokeStyle = "rgba(255,0,0,0.25)";
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.stroke();
-    ctx.closePath();
-}
+// function line(x1,y1,x2,y2){
+//     ctx.beginPath();
+//     ctx.strokeStyle = "rgba(255,0,0,0.25)";
+//     ctx.moveTo(x1,y1);
+//     ctx.lineTo(x2,y2);
+//     ctx.stroke();
+//     ctx.closePath();
+// }
 
-document.ontouchmove = function(e){
-    if( e.target === canvas ){
-        e.preventDefault(); //prevents scrolling
-    }
-    for(var i=0;i<e.touches.length;i++){
-        update(e.touches[i].pageX,e.touches[i].pageY);
-    }
-};
+// document.ontouchmove = function(e){
+//     if( e.target === canvas ){
+//         e.preventDefault(); //prevents scrolling
+//     }
+//     for(var i=0;i<e.touches.length;i++){
+//         update(e.touches[i].pageX,e.touches[i].pageY);
+//     }
+// };
 //#endregion
 
